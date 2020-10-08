04b6789e8ddb76d915108cb0218afa14d9f0323b
Fernando Ghisi <fernandoghisi@iMac-de-Fernando.local>
2020-10-08 12:06:26 -0300

    Add djangorestframework simplejwt

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/apps/rest_api/router.py
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/apps/rest_api/router.py
@@ -1,12 +1,18 @@
 # Routers provide an easy way of automatically determining the URL conf.
 from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import MeProfileView
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path("me", MeProfileView.as_view()),
 ]
 
